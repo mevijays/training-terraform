45e3d0a983f9fa468d6e0d8838c2d78f33198563
Vijay K <sharmavijay86@gmail.com>
2023-08-12 23:21:38 +0530

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,9 +1,13 @@
-
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "3.7.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.44.1"
+    }
+  }
+  cloud {
+    workspaces {
+      name = "training-terraform"
     }
   }
 }
