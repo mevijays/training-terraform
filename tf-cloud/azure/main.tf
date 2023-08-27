5deb72e1781c9543f298d2905c09013217cd47f8
Vijay Kumar <vijay@mevijay.dev>
2023-08-27 15:21:47 +0530

    fix

--- tf-cloud/azure/main.tf
+++ tf-cloud/azure/main.tf
@@ -192,7 +192,7 @@ resource "azurerm_linux_virtual_machine" "webvm" {
   data "template_file" "linux-vm-cloud-init" {
   template = file("azure-user-data.sh")
   }
-
+/*
 resource "azurerm_storage_account" "main" {
   name                     = "krlabmonstrbatch"
   resource_group_name      = azurerm_resource_group.krlabrg.name
@@ -203,6 +203,7 @@ resource "azurerm_storage_account" "main" {
     environment = "staging"
   }
 }
+*/
 variable "is_create_law" {
   type = bool
   default = false
