c10ce9c2621135841765b954304d609643a20fc7
Vijay K <sharmavijay86@gmail.com>
2024-05-12 17:46:54 +0530

    correction

--- project/eks-tf/eks_cluster.tf
+++ project/eks-tf/eks_cluster.tf
@@ -92,9 +92,9 @@ resource "aws_eks_node_group" "node" {
   instance_types  = var.node_size
 
   scaling_config {
-    desired_size = 3
-    max_size     = 9
-    min_size     = 3
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
   }
 
   depends_on = [
--- project/eks-tf/vpc.tf
+++ project/eks-tf/vpc.tf
@@ -15,7 +15,6 @@ resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnets_cidr)
   cidr_block              = element(var.public_subnets_cidr, count.index)
   map_public_ip_on_launch = false
-  depends_on              = [aws_vpc.labvpc]
   vpc_id                  = aws_vpc.labvpc.id
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   tags = {
@@ -28,14 +27,12 @@ resource "aws_subnet" "private_subnet" {
   count                   = length(var.private_subnets_cidr)
   cidr_block              = element(var.private_subnets_cidr, count.index)
   map_public_ip_on_launch = false
-  depends_on              = [aws_vpc.labvpc]
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   tags = {
     "Name" = "privatesubnet${count.index}"
   }
 }
 resource "aws_internet_gateway" "labig" {
-  depends_on = [aws_vpc.labvpc]
   vpc_id     = aws_vpc.labvpc.id
   tags = {
     "Name" = "LAB IG"
@@ -51,6 +48,10 @@ resource "aws_route_table" "public" {
     "Name" = "eks-public-route"
   }
 }
+resource "aws_eip" "nat_eip" {
+  depends_on = [aws_internet_gateway.labig]
+}
+
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
@@ -61,7 +62,6 @@ resource "aws_nat_gateway" "nat" {
 }
 resource "aws_route_table" "private" {
   vpc_id     = aws_vpc.labvpc.id
-  depends_on = [aws_nat_gateway.nat]
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.nat.id
@@ -79,7 +79,4 @@ resource "aws_route_table_association" "private" {
   count          = length(var.private_subnets_cidr)
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private.id
-}
-resource "aws_eip" "nat_eip" {
-  depends_on = [aws_internet_gateway.labig]
 }
